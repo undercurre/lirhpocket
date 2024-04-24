bfe677a2b9383f58b8debc36f6a628dc6ab19dff
undercurre <undercurre@163.com>
2024-04-25 02:25:53 +0800

    feat: 新增方法

--- src/string.ts
+++ src/string.ts
@@ -1,5 +1,8 @@
 /**
  * @description 去掉字符串中的元素标记
- * @param { Boolean } name description...
- * @return { Boolean } description...
+ * @param { String } fragment html字符串
+ * @return { String } 去掉元素标记后
  */
+
+export const removeTag = (fragment) =>
+  new DOMParser().parseFromString(fragment, "text/html").body.textContent || "";
